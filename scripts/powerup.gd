3d531f849a39be796445c54f856bd39ab2b0305b
DanNoby <dannynoby.joseph2023@vitstudent.ac.in>
2026-01-07 13:09:53 +0530

    mug and hierarchy fix

--- scripts/powerup.gd
+++ scripts/powerup.gd
@@ -6,6 +6,7 @@ func _ready():
 	var tween = create_tween().set_loops()
 	
 	tween.tween_property($mug_full2, "position:y", 0.5, 1.0).as_relative()
+	tween.tween_property($mug_full2, "position:y", -0.5, 1.0).as_relative()
 	
 	var spin_tween = create_tween().set_loops()
 	spin_tween.tween_property($mug_full2, "rotation:y", deg_to_rad(360), 2.0).as_relative()
@@ -14,4 +15,4 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("player") or body.name == "Player":
 		GameManager.heal(1) # Heal
-		queue_free() # consume mug
+		queue_free() 
